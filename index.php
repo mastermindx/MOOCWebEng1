2bc746b7600d9fc0d05951d0b3b037392e5d9fa5
Marcel Vanderschaeghe <developer@developer.local>
2013-10-16 18:49:00 +0200

    Hausaufgabe Kapitel 1 commitet

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <title>MOOC@iversity.com - Web Engineering 1: Hausaufgabe Kapitel 1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Hausaufgabe des Kapitel 1">
+    <meta name="author" content="Marcel Vanderschaeghe">
+
+    <!-- Le styles -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
+    <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="bootstrap/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="bootstrap/img/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="bootstrap/img/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="bootstrap/img/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="bootstrap/img/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="bootstrap/img/favicon.png">
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="https://iversity.org/courses/web-engineering-i-grundlagen-der-web-entwicklung" target="_blank">MOOC Web Engineering 1</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="active"><a href="#">Start</a></li>
+              <li><a href="#about">Über</a></li>
+              <li><a href="#contact">Kontakt</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Mehr... <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Thema 1</a></li>
+                  <li><a href="#">Thema 2</a></li>
+                  <li><a href="#">Thema 3</a></li>
+                  <li class="divider"></li>
+                  <li class="nav-header">Nav header</li>
+                  <li><a href="#">extra Link</a></li>
+                  <li><a href="#">noch ein extra Link</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="navbar-form pull-right">
+              <input class="span2" type="text" placeholder="obama@whitehouse.gov">
+              <input class="span2" type="password" placeholder="i_love_nsa">
+              <button type="submit" class="btn">Sign in</button>
+            </form>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <!-- Main hero unit for a primary marketing message or call to action -->
+      <div class="hero-unit">
+        <h1>Hallo Gemeinde!</h1>
+        <p>Diese Website basiert auf eine Bootstrap-Vorlage. Es wurde für diese Hausaufgabe nach eigenem Gutdünken modifizert.<br>
+		Für diese Aufgabe wurden die Stylesheets weder angepasst, noch wurden Stylesheets hinzugefügt.<br>
+		Lediglich das MOOC, der Info-Button und die unteren Themen wurden verlinkt, alle anderen Navigations-Komponenten dienen alleinig der Darstellung<br>
+		Für weitere Infos zu den Bootstrap-Vorlagen genügt ein Klick auf den Button. :-)</p>
+        <p><a href="http://getbootstrap.com/2.3.2/getting-started.html#examples" target="_blank" class="btn btn-primary btn-large">weitere Infos &raquo;</a></p>
+      </div>
+
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="span4">
+          <h2>Suchmaschine</h2>
+          <p>Quaerite, et invenietis. </p>
+          <p><a class="btn" href="http://www.google.de" target="_blank">Google &raquo;</a></p>
+        </div>
+        <div class="span4">
+          <h2>Wissensbunker</h2>
+          <p>Sapere aude! Scientia potentia est!. </p>
+          <p><a class="btn" href="http://de.wikipedia.org/wiki/Wikipedia:Hauptseite" target="_blank">Wikipedia &raquo;</a></p>
+       </div>
+        <div class="span4">
+          <h2>Bla blubb?</h2>
+          <p>Dividiert man den Sinn der Sinnlosigkeit durch die eigene Intelligenz und multipliziert dies mit der Tiefe der Erde unter einem fünf Meter tiefen Glas Wasser erhält man den Sinn des eigenen, wahrscheinlich sinnlosen Lebens, da dem besagtem Menschen so unheimlich langweilig sein muss, dass dieser freiwillig sein sinnloses Gehirn anstrengt und diesen Artikel liest.</p>
+          <p><a class="btn" href="http://www.stupidedia.org/stupi/Sinnlos" target="_blank">Sinnlos &raquo;</a></p>
+        </div>
+      </div>
+
+      <hr>
+
+      <footer>
+        <p>&copy; Marcel Vanderschaeghe 2013</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="bootstrap/js/jquery.js"></script>
+    <script src="bootstrap/js/transition.js"></script>
+    <script src="bootstrap/js/alert.js"></script>
+    <script src="bootstrap/js/modal.js"></script>
+    <script src="bootstrap/js/dropdown.js"></script>
+    <script src="bootstrap/js/scrollspy.js"></script>
+    <script src="bootstrap/js/tab.js"></script>
+    <script src="bootstrap/js/tooltip.js"></script>
+    <script src="bootstrap/js/popover.js"></script>
+    <script src="bootstrap/js/button.js"></script>
+    <script src="bootstrap/js/collapse.js"></script>
+    <script src="bootstrap/js/carousel.js"></script>
+    <script src="bootstrap/js/typeahead.js"></script>
+
+  </body>
+</html>
